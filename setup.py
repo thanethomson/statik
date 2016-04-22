034e157d0620de907d3bca933ed1d881a4cd1ef9
Thane Thomson <connect@thanethomson.com>
2016-04-22 15:57:18 +0200

    renamed src folder to statik

--- setup.py
+++ setup.py
@@ -1,6 +1,29 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
 Setup script for Statik, the static web site generator.
 """
 
-# TODO: Finish this
+from distutils.core import setup
+
+setup(
+    name="statik",
+    version="0.1.0",
+    description="General-purpose static web site generator",
+    author="Thane Thomson",
+    author_email="connect@thanethomson.com",
+    url="https://github.com/thanethomson/statik",
+    packages=["statik", "statik.filters"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Utilities",
+    ]
+)
--- statik/statik.py
+++ statik/statik.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
 Central source file for Statik, the static web site generator.
