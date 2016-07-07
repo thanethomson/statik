411a6b112459f4f7193ec8f28d42a54fdc90858f
Thane Thomson <connect@thanethomson.com>
2016-07-07 16:02:49 +0200

    fixing formatting and adding utf-8 encoding specifier

--- statik/tags.py
+++ statik/tags.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 from statik.templatetags import register
 
 
@@ -5,12 +7,13 @@ from statik.templatetags import register
 def print_context(context, *args):
     return context
 
+
 @register.simple_tag(name="ditto")
 def echo_arguments(*args, **kwargs):
     """ Echoes all parameters back as text (for debugging)
             {% ditto 1 2 3 %} => "ditto(1, 2, 3)"
     """
     args_string = ', '.join(map(lambda x: str(x), args))
-    kwargs_string = ', '.join(map(lambda k,v: "%s=%s" % (k, v), kwargs.items()))
+    kwargs_string = ', '.join(map(lambda k, v: "%s=%s" % (k, v), kwargs.items()))
     string_lst = filter(lambda x: bool(x), [args_string, kwargs_string])
     return "ditto(%s)" % ", ".join(string_lst)
