ea7b1d67446d45efc01f0db6fd22ac96caed626a
Kenton Hamaluik <hamaluik@ualberta.ca>
2016-10-14 12:36:40 -0600

    _all.yml now loads with the config's encoding

--- statik/database.py
+++ statik/database.py
@@ -164,7 +164,7 @@ class StatikDatabase(object):
     def load_model_data_collection(self, path, model):
         db_model = globals()[model.name]
         # load the collection data from the collection file
-        with open(os.path.join(path, '_all.yml'), mode='rt', encoding='utf-8') as f:
+        with open(os.path.join(path, '_all.yml'), mode='rt', encoding=self.encoding) as f:
             collection = yaml.load(f.read())
 
         if not isinstance(collection, list):
