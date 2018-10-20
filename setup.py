f6810f3de77108601cf2f7cbd83f2cb06c636cc0
Thane Thomson <connect@thanethomson.com>
2018-10-20 07:08:10 +0200

    Updates package tags to include Python 3.7 for next release

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Utilities",
     ]
