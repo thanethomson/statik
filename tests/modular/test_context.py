e0e3a9970b319fc60ff8cbc7a137f175c43a539e
Kai Chen <kaichen120@gmail.com>
2018-12-06 02:38:37 -0800

    Add environment variables to context
    Add environment variables to context so they
    may be used in views.
    
    Closes https://github.com/thanethomson/statik/issues/72

--- tests/modular/test_context.py
+++ tests/modular/test_context.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 import unittest
 
+import mock
+
 from statik.context import StatikContext
 from statik.database import StatikDatabase
 
@@ -14,3 +16,11 @@ class TestStatikContext(unittest.TestCase):
 
         result = context.build(db=StatikDatabase(models={}, data_path=''), extra={'my_var': 5})
         assert result.get('render_elm') is False
+
+    def test_env_var_in_context(self):
+        with mock.patch.dict('statik.context.os.environ',
+                             {'HOME': 'HOME!!'}):
+            context = StatikContext()
+
+            result = context.build(db=StatikDatabase(models={}, data_path=''))
+            self.assertEqual(result.get('HOME'), 'HOME!!')
